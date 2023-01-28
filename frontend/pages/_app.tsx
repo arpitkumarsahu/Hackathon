d4d48dd79e7c1793f16051a11ae91d3fbbcb56a4
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-01-28 15:17:27 +0530

    navbar

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux'
+// import { store } from '../redux/store'
+
+export default function App({ Component, pageProps }) {
+  return (
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
 }
